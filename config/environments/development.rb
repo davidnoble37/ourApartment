be9d56eb0b13a1041a5bb5636ca9b9b61a35d61a
David Noble <davidnoble37@gmail.com>
2017-06-29 18:54:04 -0700

    geo2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_host_name: ENV['S3_HOST_NAME'],
-    s3_region: 'us-east-2',  # or ENV['AWS_REGION']; Aws.config[:region]
+    s3_region: Aws.config[:region], #'us-east-2',  # or ENV['AWS_REGION']; Aws.config[:region]
     s3_credentials: {
       bucket: ENV['S3_BUCKET_NAME'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,16 +96,14 @@ Rails.application.configure do
   config.paperclip_defaults = {
   storage: :s3,
   s3_host_name: ENV['S3_HOST_NAME'],
-  s3_region: Aws.config[:region], #'us-east-2',  # or ENV['AWS_REGION']
+  s3_region: Aws.config[:region], #'us-east-2',  # or ENV['AWS_REGION']; Aws.config[:region]
   s3_credentials: {
     bucket: ENV['S3_BUCKET_NAME'],
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-
   }
 }
 
-Aws.config[:region]
-ENV['AWS_REGION']
+
 
 end
